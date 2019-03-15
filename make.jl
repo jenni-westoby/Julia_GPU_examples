e8d159695247b20bfec9bf9e678bdb3dc1cf970e
jenni-westoby <jenniwestoby@gmail.com>
2019-03-15 12:04:10 +0000

    Another attempt to get web support

--- make.jl
+++ make.jl
@@ -5,7 +5,7 @@ end
 
 makedocs(modules=[MyModule],
 build = "build",
-repo = "https://github.com/jenni-westoby/Julia_GPU_examples.git",
+repo = "https://github.com/jenni-westoby/Julia_GPU_examples.jl.git",
 pages = Any[
 "Home" => "index.md",
 "Getting Started" => Any["GPU_background.md","Accessing_GPUs.md",
@@ -18,6 +18,6 @@ pages = Any[
 "About the Author" => "About_the_author.md"])
 
 deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
-repo = "https://github.com/jenni-westoby/Julia_GPU_examples.git",
+repo = "https://github.com/jenni-westoby/Julia_GPU_examples.jl.git",
 julia  = "1.0.3",
 osname = "linux")
