b99c706a473821e71256be3f7d9b5762262b046d
autodocs <autodocs>
2019-03-20 21:30:38 +0000

    build based on 345c775

--- dev/search_index.js
+++ dev/search_index.js
@@ -77,7 +77,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Accessing GPUs",
     "title": "Accessing GPUs",
     "category": "section",
-    "text": "You basically have three options for accessing GPUs:Access GPUs on a commercial cloud based platform eg. AWS\nAccess GPUs using a HPC you have access to through your company/academic institution.\nUse the GPU in your own computer"
+    "text": "You basically have three options for accessing GPUs:Access GPUs on a commercial cloud based platform e.g. AWS.\nAccess GPUs using a HPC you have access to through your company/academic institution.\nUse the GPU in your own computer"
 },
 
 {
@@ -93,7 +93,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Accessing GPUs",
     "title": "Option 2",
     "category": "section",
-    "text": "If you are going for option 2, talk to the HPC administrator to find out how to get access to the GPUs and check that the CUDA toolkit are already installed."
+    "text": "If you are going for option 2, talk to the HPC administrator to find out how to get access to the GPUs and check that the CUDA toolkit is already installed."
 },
 
 {
@@ -101,15 +101,15 @@ var documenterSearchIndex = {"docs": [
     "page": "Accessing GPUs",
     "title": "Option 3",
     "category": "section",
-    "text": "If you are going for option 3, things are about to get interesting.These are some instructions you can try following to get the GPU in your computer working in a way that Julia will be able to interact with it. I make no promises that they will work and will not offer support if they fail for you. These instructions assume you are working in a Linux environment. I don\'t know how to make your GPU work in a Windows or Mac environment.Step 1: Check that your computer has a GPU.Step 2: Check that your computer has a GPU that is actually supported by CUDA (CUDA is only supported on GPUs made by NVIDIA, and is not supported on old NVIDIA GPUs).Step 3: Replace your operating system with a fresh install of Ubuntu 18.04. No, I am not joking. Remember to back up your files before you install a new operating system just in case something goes wrong.Step 4: Open a terminal and type in the following commands:sudo add-apt-repository ppa:graphics-drivers/ppa\n\nsudo apt update\n\nsudo ubuntu-drivers autoinstall\nStep 5: Reboot your computerStep 6: Open a terminal and type in the following:sudo apt install nvidia-cuda-toolkit gcc-6\n\nnvcc --version\nIf your install has worked, the final command should print out some version information.Step 7: Assuming all of the above worked, install the latest version of Julia and continue working through this tutorial.Disclaimer The above are NOT authoritative instructions on how to install the CUDA toolkit, they are instructions that worked for me. You should visit the NVIDIA website for authoritative instructions.Disclaimer I am writing these instructions in spring 2019. If you are reading this disclaimer and the year is 2021 or greater, these instructions may be woefully out of date and better ignored."
+    "text": "If you are going for option 3, things are about to get interesting.These are some instructions you can try following to get the GPU in your computer working in a way that will allow Julia to interact with it. However, I have not carried out extensive testing on different hardware platforms, and thus I make no promises that they will work and I will not offer support if they fail. These instructions assume you are working in a Linux environment. I do not know how to make your GPU work in a Windows or Mac environment.Step 1: Check that your computer has a GPU.Step 2: Check that your computer has a GPU that is actually supported by CUDA (CUDA is only supported on GPUs made by NVIDIA, and is not supported on older NVIDIA GPUs).Step 3: Replace your operating system with a fresh install of Ubuntu 18.04. No, I am not joking. Remember to back up your files before you install a new operating system just in case something goes wrong.Step 4: Open a terminal and type in the following commands:sudo add-apt-repository ppa:graphics-drivers/ppa\n\nsudo apt update\n\nsudo ubuntu-drivers autoinstall\nStep 5: Reboot your computerStep 6: Open a terminal and type in the following:sudo apt install nvidia-cuda-toolkit gcc-6\n\nnvcc --version\nIf your install has worked, the final command should print out some version information.Step 7: Assuming all of the above worked, install the latest version of Julia and continue working through this tutorial.Disclaimer The above are NOT authoritative instructions on how to install the CUDA toolkit, they are instructions that worked for me. You should visit the NVIDIA website for authoritative instructions.Disclaimer I am writing these instructions in spring 2019, pre-Brexit. If you are reading this disclaimer and the year is 2021 or greater, these instructions may be woefully out of date and better ignored. I would also be grateful if you travel back in time and fix parliament."
 },
 
 {
-    "location": "Accessing_GPUs/#Surely-I-don\'t-really-need-to-install-a-new-operating-system-1",
+    "location": "Accessing_GPUs/#Surely-I-don\'t-really-need-to-install-a-new-operating-system?-1",
     "page": "Accessing GPUs",
-    "title": "Surely I don\'t really need to install a new operating system",
+    "title": "Surely I don\'t really need to install a new operating system?",
     "category": "section",
-    "text": "The CUDA toolkit and drivers are notoriously difficult to install. It might sound a bit mad, but installing a fresh version of Ubuntu 18.04 is the most reliable method for installing CUDA that I have found. Note, steps 4-6 are taken from the second answer here."
+    "text": "The CUDA toolkit and drivers are notoriously difficult to install. It might sound a bit mad, but in my experience installing a fresh version of Ubuntu 18.04 is the most reliable method for installing CUDA. Note, steps 4-6 are taken from the second answer here."
 },
 
 {
