0baa35d4e075ba078d8d0a71e50c8ffe3892d0a9
autodocs <autodocs>
2019-03-21 20:58:42 +0000

    build based on d872718

--- dev/search_index.js
+++ dev/search_index.js
@@ -397,7 +397,7 @@ var documenterSearchIndex = {"docs": [
     "page": "About the Author",
     "title": "Jenni Westoby",
     "category": "section",
-    "text": "(Image: )I am a PhD student studying bioinformatics (computational biology) at the University of Cambridge and the Wellcome Trust Sanger Institute. I am supervised by Professor Anne Ferguson-Smith and Dr Martin Hemberg. I got interested in GPU software development because of its potential to accelerate bioinformatics software. I hope you enjoyed reading this tutorial and that you found it useful."
+    "text": "(Image: )I am a PhD student studying bioinformatics (computational biology) at the University of Cambridge and the Wellcome Sanger Institute. I am supervised by Professor Anne Ferguson-Smith and Dr Martin Hemberg. I got interested in GPU software development because of its potential to accelerate bioinformatics software. I hope you enjoyed reading this tutorial and that you found it useful."
 },
 
 ]}
